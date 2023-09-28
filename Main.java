3e881de1731a0e7653deeb9377dd9b6177f5c9e0
Tomás Achával Vinuesa <117323830+TomasAchavalVinuesa@users.noreply.github.com>
2023-09-28 18:31:30 -0300

    se añadió un normalizar de números generados, un caso práctico en main  y la teoría de prueba promedio

--- Main.java
+++ Main.java
@@ -3,9 +3,14 @@ public class Main {
         GeneradorCongruencialMixto generador1 = new GeneradorCongruencialMixto(128, 13, 127, 42);
         int periodo = generador1.obtenerPeriodo();
         System.out.println(periodo);
-
+        /*
         double[] a = new double[periodo];
         a=generador1.obtenerMuestraParcial(periodo);
+        for (int i = 0; i< periodo; i++){
+            System.out.println("Número generado: "+"\u001B[32m" + a[i] + "\u001B[0m");
+        }*/
+
+        double[] a = normalizadorNumeros.normalizar(generador1, periodo, 8);
         for (int i = 0; i< periodo; i++){
             System.out.println("Número generado: "+"\u001B[32m" + a[i] + "\u001B[0m");
         }
---
+++ normalizadorNumeros.java
@@ -0,0 +1,12 @@
+public class normalizadorNumeros {
+
+    public static double[] normalizar(Generador generador, int tamanoMuestra, int cantidadDeCeros ){
+        double potencia = Math.pow(10.0, cantidadDeCeros); //se deberá añadir control para que cantidadDeCeros sea mayor o igual que 0
+        double[] numerosTransformar = generador.obtenerMuestraParcial(tamanoMuestra);
+        for (int i = 0; i<tamanoMuestra; i++){
+            double auxiliar = numerosTransformar[i] / (generador.moduloM-1);
+            numerosTransformar[i] = (Math.round(auxiliar * potencia)/potencia);
+        }
+        return  numerosTransformar;
+    }
+}
